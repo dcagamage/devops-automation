aeba478592e40f914b49f752adca8229f4c08a32
Denuka Gamage <dca.gamage10@gmail.com>
2025-08-24 13:00:43 +0530

    first commit

---
+++ src/test/java/com/javateche/devops_integration/DevopsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javateche.devops_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
